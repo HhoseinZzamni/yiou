22c0e5db5382e26383b7b8c8ff46eba72660aba5
HhoseinZzamni <138737378+HhoseinZzamni@users.noreply.github.com>
2024-01-13 20:00:55 +0100

    Update aysan.py

--- aysan.py
+++ aysan.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([80, 11, 901, 95, 100, 110, 120])
+x = np.array([80, 11, 901, 95, 100, 120])
 y = np.array([240, 250, 260, 270, 280, 300, 310, 330])
 
 plt.title("Sports Watch Data")
