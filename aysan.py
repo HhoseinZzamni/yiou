b48b0b43aa6966ce3febd3e673da71cb6f1b6b48
HhoseinZzamni <138737378+HhoseinZzamni@users.noreply.github.com>
2024-01-31 17:19:06 +0100

    Update aysan.py

--- aysan.py
+++ aysan.py
@@ -21,7 +21,7 @@ x = np.array([80, 85, 90, 95, 100, 10, 110, 115, 12, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
 plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
+plt.xlabel("Averae Pulse")
 plt.ylabel("Calorie Burnage")
 
 plt.plot(x, y)
