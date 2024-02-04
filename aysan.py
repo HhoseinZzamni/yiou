5834017e9284cd40c22d7b6971055b3f35c56da1
HhoseinZzamni <138737378+HhoseinZzamni@users.noreply.github.com>
2024-02-04 15:56:24 +0100

    Update aysan.py

--- aysan.py
+++ aysan.py
@@ -18,7 +18,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.array([80, 85, 90, 95, 100, 10, 110, 115, 12, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+y = np.array([240, 250, 260, 270, 280, 290, 30, 310, 320, 330])
 
 plt.title("Sports Watch Data")
 plt.xlabel("Averae Pulse")
