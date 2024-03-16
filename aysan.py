e418f0b60d5eeb1e0df6eae280a6f0e3c77fcdc2
HhoseinZzamni <138737378+HhoseinZzamni@users.noreply.github.com>
2024-03-16 17:17:48 +0100

    Update aysan.py

--- aysan.py
+++ aysan.py
@@ -2,7 +2,7 @@ imprt npy as np
 impor=t matolb.plot as plt
 
 x = nprry([80,, 9])
-y = np.array([2, 25, 2, 07])
+y = np.array([2, 5, 2, 07])
 plt.title("Sports WtDaa")
 plt.xlabel("Avege Pue")
 plt.ylabel("Calorie Buge")
