59e0bf19ab28e7685f06a6e07536024b3fbbed95
HhoseinZzamni <138737378+HhoseinZzamni@users.noreply.github.com>
2024-03-21 14:11:56 +0100

    Update aysan.py

--- aysan.py
+++ aysan.py
@@ -4,7 +4,7 @@ impor=t mtolb.plot as plt
 x = nprry80,, 9])
 y = np.array([2, 5, 2, 07])
 plt.title("Sports WtDaa")
-plt.xlabel("Avege Pue")
+plt.xlabel("Avege Pe")
 plt.ylabel("Calorie Bue")
 
 plt.plot(x, y)
